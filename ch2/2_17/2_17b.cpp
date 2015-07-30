acf5b91a8113afae7b9a5f960d4539aede3885d0
PunyTitan <pinjiahe@gmail.com>
2015-07-30 14:52:52 +0800

    bug fix
    not only the pre-sequence sum should be larger, the index should also be
    larger.

--- ch2/2_17/2_17b.cpp
+++ ch2/2_17/2_17b.cpp
@@ -5,7 +5,21 @@
 
 using namespace std;
 
-int posSumSubseq(vector<int> & a)
+class Item
+{
+public:
+	int value;
+	int index;
+};
+
+
+bool cmp(const Item & i1, const Item & i2)
+{
+	return i1.value<i2.value;
+}
+
+
+int posSumSubseq(vector<Item> & a)
 {
 	int smallest = numeric_limits<int>::max();
 	int sum = 0;
@@ -15,17 +29,29 @@ int posSumSubseq(vector<int> & a)
 
 	for(int i=0; i<a.size(); i++)
 	{
-		sum += a[i];
-		a[i] = sum;
+		sum += a[i].value;
+		a[i].value = sum;
 	}
 
-	sort(a.begin(), a.end());
+	sort(a.begin(), a.end(), cmp);
 
-	for(int i=0; i<a.size()-1; i++)
+	for(int i=0; i<a.size(); i++)
+		if(a[i].value<smallest)
+			smallest = a[i].value;
+
+	for(int i=0; i+1<a.size(); i++)
 	{
-		sum = a[i+1] - a[i];
-		if (sum<smallest)
-			smallest = sum;
+		for (int j=i+1; j<a.size(); j++ )
+		{
+			if(a[i].index<a[j].index)
+			{
+				sum = a[j].value - a[i].value;
+				break;
+			}
+		}
+		if(sum<smallest)
+			smallest = sum;		
+
 	}
 
 	return smallest;
@@ -33,8 +59,16 @@ int posSumSubseq(vector<int> & a)
 
 int main(int argc, char const *argv[])
 {
-	int a[10] = {2,3,5,7,-1,-4,51,-2,-6,1};
-	vector<int> aVec(a, a + sizeof(a)/sizeof(a[0]));
+	int a[10] = {5,99,5,7,6,99,-97,8,5,7};
+	Item aItem[10] = {};
+
+	for(int i=0; i<sizeof(a)/sizeof(a[0]); i++)
+	{
+		aItem[i].value = a[i];
+		aItem[i].index = i;
+	}
+
+	vector<Item> aVec(aItem, aItem + sizeof(aItem)/sizeof(aItem[0]));
 
 	cout<<posSumSubseq(aVec);	
 	return 0;
