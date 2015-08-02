b64fb6caeab73912342e7855be4b6d610b93ec8f
PunyTitan <pinjiahe@gmail.com>
2015-08-02 21:21:38 +0800

    bug_fix and implementation of iterator/const_iterator
    "Bug of" inheritance of template classes
    http://gcc.gnu.org/onlinedocs/gcc/Name-lookup.html

--- ch3/listDouble.h
+++ ch3/listDouble.h
@@ -3,60 +3,108 @@
 template<typename Object>
 class listDouble
 {
+private:
+	struct Node
+	{
+		Object item;
+		Node *left;
+		Node *right;
+
+		Node(const Object & obj = Object(), Node *lp = NULL, Node *rp = NULL)
+		:item(obj), left(lp), right(rp){}
+	};
+
+
 public:
 
 	class const_iterator
 	{
-		const_iterator()
+		const_iterator():current(NULL)
 		{}
 
-		~const_iterator()
+		const_iterator & operator++()
 		{
-			delete [] const_itr;
-		}
-
-		const_iterator operator++()
-		{
-
+			current = current->right;
+			return *this;
 		}
 
 		const_iterator operator++(int)
 		{
-
+			const_iterator rtr_itr = *this;
+			++(*this);
+			return rtr_itr;
 		}
 
 		const Object & operator*() const
 		{
-
+			return retrieve();
 		}
 
-		bool operator==(const Object & rhs) const
+		bool operator==(const const_iterator & rhs) const
 		{
-
+			if(current == rhs.current)
+				return true;
 		}
 
-		bool operator!=(const Object & rhs) const
+		bool operator!=(const const_iterator & rhs) const
 		{
-
+			return !(*this == rhs);
 		}
 
 
 	protected:
-		Object* const_itr;
+		Node *current;
 
-		const_iterator(Node *ptr):const_itr(ptr)
+		const_iterator(Node *ptr):current(ptr)
 		{}
 
-		Object & retrieve()
+		Object & retrieve() const
 		{
-
+			return current->item;
 		}
 
 	friend class listDouble<Object>;
 	};
 
-	class iterator:const_iterator
+	/*
+	In this class, I use this->current instead of current because the compiler cannot handle template well in this condition. (Or we can say that the compiler has its design choice)
+	Related explaination: http://gcc.gnu.org/onlinedocs/gcc/Name-lookup.html
+	*/
+	class iterator : public const_iterator
 	{
+		public:
+		iterator()
+		{}
+
+		iterator & operator++()
+		{
+			this->current = this->current->right;
+			return *this;
+		}
+
+		iterator operator++(int)
+		{
+			iterator rtr_itr = *(this->current);
+			++(*this);
+			return rtr_itr;
+		}
+
+		Object & operator*()
+		{
+			return this->retrieve();
+		}
+
+		const Object & operator*() const
+		{
+			return const_iterator::operator*();
+		}
+
+
+	protected:
+		iterator(Node *ptr):const_iterator(ptr)
+		{}
+
+		friend class listDouble<Object>;
 
 	};
 
@@ -176,15 +224,6 @@ public:
 
 
 private:
-	struct Node
-	{
-		Object item;
-		Node *left;
-		Node *right;
-
-		Node(const Object & obj = Object(), Node *lp = NULL, Node *rp = NULL)
-		:item(obj), left(lp), right(rp){}
-	};
 
 	int sizeV;
 	Node *head;
@@ -195,8 +234,13 @@ private:
 /*
 key points:
 
-Functions, which will return an item in the collection, should be implemented with an accessor and a modifier.
-Two kinds of iterator, so two kinds of begin() and end()
-The usage of friend in iterator class
+1. Functions, which will return an item in the collection, should be implemented with an accessor and a modifier.
+2. Two kinds of iterator, so two kinds of begin() and end()
+3. The usage of friend in iterator class
+4. The constructor of const_iterator with zero parameter should initialize the pointer to NULL
+5. operator++ in const_iterator should return itself, not a pointer
+6. when you implement a new operator*() function in iterator, you should also implement a operator*() const, because the original one will be hidden otherwise.
+7. notice the difference between the return value of operator++() and operator++(int)
+
 */
 
