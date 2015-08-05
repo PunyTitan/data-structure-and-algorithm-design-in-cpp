a01ab0c56e71c95f609a8fed21a29a96154506a0
PunyTitan <pinjiahe@gmail.com>
2015-08-05 11:08:56 +0800

    Single linked list completed
    The pointer beforeTail is important to reduce time complexity.

--- ch3/listSingle.h
+++ ch3/listSingle.h
@@ -32,6 +32,22 @@ public:
 	 		return rtr_irt;
 	 	}
 
+	 	const_iterator operator--()
+	 	{
+	 		const_iterator itr = beforeBegin();
+
+	 		while(itr.current->next != current)
+	 			++itr;
+	 		return itr;
+	 	}
+
+	 	const_iterator operator--(int)
+	 	{
+	 		const_iterator itr = *this;
+	 		--(*this);
+	 		return itr;
+	 	}
+
 	 	const Object & operator*() const
 	 	{
 	 		return retrieve();
@@ -63,6 +79,8 @@ public:
 			return current->item;
 		}
 
+		friend class listSingle<Object>;
+
 	};
 
 	class iterator:public const_iterator
@@ -84,6 +102,22 @@ public:
 			return rtr_irt;
 		}
 
+		iterator operator--()
+	 	{
+	 		iterator itr = beforeBegin();
+
+	 		while(itr.current->next != this->current)
+	 			++itr;
+	 		return itr;
+	 	}
+
+	 	iterator operator--(int)
+	 	{
+	 		iterator itr = *this;
+	 		--(*this);
+	 		return itr;
+	 	}
+
 		Object & operator*()
 		{
 			return retrieve();
@@ -101,15 +135,152 @@ public:
 			return this->current->item;
 		}
 		
+		friend class listSingle<Object>;
 	};
 
-	listSingle();
-	~listSingle();
+	listSingle()
+	{
+		init();
+	}
+
+	~listSingle()
+	{
+		clear();
+		delete head;
+		delete tail;
+	}
+
+	void init()
+	{
+		head = new Node();
+		tail = new Node();
+		beforeTail = head;
+		head->next = tail;
+		sizeV = 0;
+	}
+
+	void clear()
+	{
+		while(!empty())
+			pop_front();
+	}
+
+	//insert to the position after iterator, different from the insert in double linked list because of complexity
+	iterator insert(const Object & obj, iterator itr)
+	{
+		Node * ptr = new Node(obj, itr.current->next);
+		itr.current->next = ptr;
+		if(itr.current == beforeTail)
+			beforeTail = ptr;
+		return iterator(ptr);
+	}
+
+	//erase the position after the iterator, different from the insert in double linked list because of complexity
+	iterator erase(iterator itr)
+	{
+		Node * ptr = itr.current->next;
+		itr.current->next = ptr->next;
+		if(ptr == beforeTail)
+			beforeTail = itr.current;
+		delete ptr;
+
+		return itr;
+	}
+
+	iterator erase(iterator from, iterator to)
+	{
+		while(from.current->next != to.current)
+		{
+			erase(from);
+		}
+	}
+
+	const_iterator begin() const
+	{
+		return const_iterator(head->next);	
+	}
+
+	iterator begin()
+	{	
+		return iterator(head->next);
+	}
+
+	const_iterator end() const
+	{
+		return const_iterator(tail);
+	}
+
+	iterator end()
+	{
+		return iterator(tail);
+	}	
+
+	const_iterator beforeBegin() const
+	{
+		return const_iterator(head);
+	}
+
+	iterator beforeBegin()
+	{
+		return iterator(head);
+	}
+
+	const_iterator beforeEnd() const
+	{
+		return const_iterator(beforeTail);
+	}
+
+	iterator beforeEnd()
+	{
+		return iterator(beforeTail);
+	}
+
+	bool empty()
+	{
+		return size() == 0;
+	}
+
+	int size()
+	{
+		return sizeV;
+	}
+
+	iterator push_back(const Object & obj)
+	{
+		insert(obj, beforeEnd());
+	}
+
+	iterator push_front(const Object & obj)
+	{
+		insert(obj, beforeBegin());
+	}
+
+	//O(log(n))
+	iterator pop_back()
+	{
+		erase(--beforeTail());
+	}
+
+	iterator pop_front()
+	{
+		erase(beforeBegin());
+	}
+
+	const Object & back()
+	{
+		return *(beforeTail());
+	}
+
+	const Object & front()
+	{
+		return *(begin());
+	}
 	
 private:
 	int sizeV;
 	Node * head;
 	Node * tail;
+	Node * beforeTail;
 };
 
 
@@ -118,4 +289,5 @@ key points:
 
 1. In constructor, remember to add default value for the data members, usually you will need it.
 2. Mark "const" to accessors
+3. The iterator begin() function is not an accessor.
 */
