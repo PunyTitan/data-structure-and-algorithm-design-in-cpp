c74c67b7eaed320c8c3841b534ebe6489525800f
PunyTitan <pinjiahe@gmail.com>
2015-08-15 17:38:11 +0800

    3.23 (c) completed

--- ch3/3_23.cpp
+++ ch3/3_23.cpp
@@ -7,6 +7,31 @@
 
 using namespace std;
 
+int opLevel(string str)
+{
+	int quality;
+	switch(str[0])
+	{
+		case '+':
+		case '-':
+			quality = 1;
+			break;
+		case '*':
+		case '/':
+			quality = 2;
+			break;
+		case '^':
+			quality = 3;
+			break;
+		case ' ':
+			quality = 0;
+			break;
+	}
+	
+
+	return quality;
+}
+
 vector<string> split(string str, char delimiter)
 {
 	vector<string> rtr;
@@ -19,6 +44,102 @@ vector<string> split(string str, char delimiter)
 	}
 
 	return rtr;
+}
+
+
+//keep to stacks, one for expressions, one for the maximum operator in that expression(considering the parenthesis)
+string postToIn(string str)
+{
+	stack<string> expS;
+	stack<string> operS;
+	vector<string> inputV = split(str, ' ');
+	double result;
+	string token, expr1, expr2, op1, op2;
+	int parenthesis; 	//-1 for left, 0 for no, 1 for right
+
+	for(vector<string>::const_iterator itr = inputV.begin(); itr != inputV.end(); ++itr)
+	{
+		token = *itr;
+		result = atof(token.c_str());
+
+		if(result != 0.0)
+		{
+			expS.push(token);
+			operS.push(" ");
+		}
+		else if(token[0] == '0')
+		{
+			expS.push(token);
+			operS.push(" ");
+		}
+		else
+		{
+			switch(token[0])
+			{
+				case '+':
+				case '-':
+					expr2 = expS.top();
+					expS.pop();
+					expr1 = expS.top();					
+					expS.pop();
+					expS.push(expr1 + " " + token + " " + expr2);
+					
+					op2 = operS.top();
+					operS.pop();
+					op1 = operS.top();
+					operS.pop();
+					operS.push(token);
+					
+					break;
+				case '*':
+				case '/':
+				case '^':
+					expr2 = expS.top();
+					expS.pop();
+					expr1 = expS.top();					
+					expS.pop();					
+					
+					op2 = operS.top();
+					operS.pop();
+					op1 = operS.top();
+					operS.pop();
+					operS.push(token);
+
+					if(op1 == " " && op2 == " ")
+					{
+						parenthesis = 0;
+					}						
+					else if(op1 == " ")
+					{
+						if(opLevel(op2)<opLevel(token))
+							parenthesis = 1;						
+						else
+							parenthesis = 0;
+					}
+					else
+					{
+						if(opLevel(op1)<opLevel(token))
+							parenthesis = -1;						
+						else
+							parenthesis = 0;
+					}
+
+					if(parenthesis == 1)
+						expS.push(expr1 + " " + token + " ( " + expr2 + " )");
+					else if(parenthesis == -1)
+						expS.push("( " + expr1 + " ) " + token + " " + expr2);
+					else
+						expS.push(expr1 + " " + token + " " + expr2);					
+					break;
+				
+			}
+		}
+	}
+
+	if(expS.size() != 1)
+		std::cout<<"Error: There are more than 1 final operations"<<std::endl;
+	
+	return expS.top();
 }
 
 string inToPost(string str)
@@ -39,7 +160,7 @@ string inToPost(string str)
 		{
 			resultV.push_back(token);
 		}
-		else if(token[0] == 0)
+		else if(token[0] == '0')
 		{
 			resultV.push_back(token);
 		}
@@ -103,11 +224,19 @@ string inToPost(string str)
 
 int main(int argc, char const *argv[])
 {
-	string test = "4.99 * 1.06 + 5.99 + 6.99 * 1.06"; 
+	string test1 = "4.99 * 1.06 + 5.99 + 6.99 * 1.06"; 
 	string test2 = "1 + 2 * 3 + ( 4 * 5 + 6 ) * 7";
 	string test3 = "1 + 2 * 3 + ( 4 * 5 ^ 6 ) * 7";
-	cout<<inToPost(test)<<endl;	
+	string test_1 = "4.99 1.06 * 5.99 + 6.99 1.06 * +";
+	string test_2 = "1 2 3 * + 4 5 * 6 + 7 * +";
+	string test_3 = "1 2 3 * + 4 5 6 ^ * 7 * +";
+
+	cout<<inToPost(test1)<<endl;	
 	cout<<inToPost(test2)<<endl;
 	cout<<inToPost(test3)<<endl;
+
+	cout<<postToIn(test_1)<<endl;	
+	cout<<postToIn(test_2)<<endl;
+	cout<<postToIn(test_3)<<endl;
 	return 0;
 }
